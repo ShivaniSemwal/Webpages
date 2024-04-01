5a67aa40cc37e2262c36804f754bf6a9914af1ec
ShivaniSemwal <49102687+ShivaniSemwal@users.noreply.github.com>
2024-04-01 12:58:00 -0230

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,10 +23,10 @@ LINKS = (
 )
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+#SOCIAL = (
+#    ("You can add links in your config file", "#"),
+#    ("Another social link", "#"),
+#)
 
 DEFAULT_PAGINATION = 10
 
